4502987fa4e63cc0b61a2946ec09383e05518d24
Gen Tamura <gen.tamura84@gmail.com>
2016-01-05 10:40:05 +0000

    add backbone

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+import Backbone from 'backbone'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import CommentBox from './components/CommentBox'
+import $ from 'jquery'
+
+Backbone.$ = $;
+console.log(Backbone);
 
 ReactDOM.render(
   <CommentBox url="comments.json" pollInterval={2000} />,
